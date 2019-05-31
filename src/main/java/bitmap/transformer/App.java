012c9f1df2f04974ad3275f49718de342c606ea9
river-ceanne <snvencer@gmail.com>
2019-05-31 09:58:44 -0700

    saving...

--- src/main/java/bitmap/transformer/App.java
+++ src/main/java/bitmap/transformer/App.java
@@ -9,6 +9,11 @@ public class App {
     }
 
     public static void main(String[] args) {
+
         System.out.println(new App().getGreeting());
+    }
+
+    public static void transformBMP(){
+        
     }
 }
--- src/test/java/bitmap/transformer/AppTest.java
+++ src/test/java/bitmap/transformer/AppTest.java
@@ -10,5 +10,7 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+
     }
 }
